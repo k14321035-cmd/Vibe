ffc9718451dc5cc318ff3518fac0ad36404d1e53
k14321035-cmd <k14321035@gmail.com>
2026-02-23 17:48:54 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,46 @@ import LobbyPage from './pages/LobbyPage';
 import RoomPage from './pages/RoomPage';
 import ProtectedRoute from './components/ProtectedRoute';
 
+import { App as CapacitorApp } from '@capacitor/app';
+import { Capacitor } from '@capacitor/core';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+function BackButtonHandler() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!Capacitor.isNativePlatform()) return;
+
+    const setupListener = async () => {
+      const listener = await CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+        // Exit app if on root or lobby
+        if (location.pathname === '/' || location.pathname === '/lobby') {
+          CapacitorApp.exitApp();
+        } else if (canGoBack) {
+          navigate(-1);
+        } else {
+          CapacitorApp.exitApp();
+        }
+      });
+      return listener;
+    };
+
+    const listenerPromise = setupListener();
+
+    return () => {
+      listenerPromise.then(l => l.remove());
+    };
+  }, [location, navigate]);
+
+  return null;
+}
+
 export default function App() {
 
   return (
     <Router>
+      <BackButtonHandler />
       <AuthProvider>
         <Routes>
           <Route path="/" element={<LoginPage />} />
